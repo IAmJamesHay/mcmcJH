3aa5c5985bc8fdb5c12718e1108f5f4e0232ead3
james <jameshay218@gmail.com>
2015-06-09 15:49:59 +0100

    fixing

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -205,10 +205,10 @@ generate_prediction_intervals_1.2 <- function(mcmc_chains,burnin=NULL,times,runs
 #' @param MODEL_FUNCTION a pointer to an existing R function that is used to calculate the model trajectories
 #' @return a list containing the following items:
 #' \itemize{
-#' \item{model_data}{a data frame with the original data (ie. fitted) for use by a plotting function}
-#' \item{prediction_data}{a data frame with the best fitting model's data for use by a plotting function}
-#' \item{lower_prediction_bounds}{a data frame with the necessary information required to plot the lower prediction bound}
-#' \item{upper_prediction_bounds}{a data frame with the necessary information required to plot the upper prediction bound}
+#' \item{model_data: }{a data frame with the original data (ie. fitted) for use by a plotting function}
+#' \item{prediction_data: }{a data frame with the best fitting model's data for use by a plotting function}
+#' \item{lower_prediction_bounds: }{a data frame with the necessary information required to plot the lower prediction bound}
+#' \item{upper_prediction_bounds: }{a data frame with the necessary information required to plot the upper prediction bound}
 #' }
 #' @seealso \code{\link{generate_prediction_intervals_1.2}}
 #' @export
